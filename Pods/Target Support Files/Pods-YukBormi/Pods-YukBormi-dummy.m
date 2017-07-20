5a4e0d3c3c6e3d8b9db7e03208cfbcc341721d58
Khusan Saidvaliev <saidhusan.saidvaliev@gmail.com>
2017-07-20 19:14:06 +0500

    start

---
+++ Pods/Target Support Files/Pods-YukBormi/Pods-YukBormi-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YukBormi : NSObject
+@end
+@implementation PodsDummy_Pods_YukBormi
+@end
---
+++ Pods/Target Support Files/Pods-YukBormiUITests/Pods-YukBormiUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YukBormiUITests : NSObject
+@end
+@implementation PodsDummy_Pods_YukBormiUITests
+@end
